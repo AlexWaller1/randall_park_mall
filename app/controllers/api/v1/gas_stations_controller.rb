1d5f9e9373ce9fd739aed9d2c97a20ead4962473
Alex Waller <alex@artofalexwaller.com>
2021-03-08 10:59:50 -0500

    controller actions

--- app/controllers/api/v1/gas_stations_controller.rb
+++ app/controllers/api/v1/gas_stations_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::GasStationsController < ApplicationController
+
+    def index
+        gas_stations = GasStation.all
+        render json: gas_stations
+    end
+
+    def create
+        gas_station = GasStation.new(gas_station_params)
+        if gas_station.save
+            render json: gas_station, status: :accept
+        else
+            render json: {errors: gas_station.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def gas_station_params
+        params.require(:gas_station).permit(:name, :address, :minimart, :state_id)
+    end
 end
